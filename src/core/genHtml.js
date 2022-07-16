d2534d0ed7d08f8bd397c62bf2236fdd2b5655bb
PoneyClairDeLune <88174309+PoneyClairDeLune@users.noreply.github.com>
2022-07-16 15:56:44 +0000

    First viable product, but is feature incomplete.

--- src/core/genHtml.js
+++ src/core/genHtml.js
@@ -1,7 +1,13 @@
 "use strict";
 
-let wrapHtml = function (title, content) {
-	return `<!DOCTYPE html><head><meta content="true" name="HandheldFriendly"/><title>${title}</title><style>*{font-family:sans-serif;color:#555}body{position:absolute;width:100%;height:100%;background:#fff;margin:0;display:flex;align-items:center;justify-content:center}#a{width:max-content;max-width:min(450px,80vw)}#b{text-align:center!important;font-weight:bold;font-size:2em;color:#333}#c{color:#333}#a>div{hyphens:auto;text-align:justify;padding:4px}</style></head><body lang="en"><div id="a"><div id="b">${title}</div><div>${content}</div></div></body>`;
+let wrapHtml = function (status = 200, title = "Lorem", content = "Blank.") {
+	return new Response(`<!DOCTYPE html><head><meta content="true" name="HandheldFriendly"/><title>${title}</title><style>*{font-family:sans-serif;color:#555}body{position:absolute;width:100%;height:100%;background:#fff;margin:0;display:flex;align-items:center;justify-content:center}#a{width:max-content;max-width:min(450px,80vw)}#b{text-align:center!important;font-weight:bold;font-size:2em;color:#333}#c{color:#333}#a>div{hyphens:auto;text-align:justify;padding:4px}a{text-decoration:none}</style></head><body lang="en"><div id="a"><div id="b">${title}</div><div>${content}</div></div></body>`, {
+		status: status,
+		headers: {
+			"Server": "Cloud Hop",
+			"Content-Type": "text/html"
+		}
+	});
 };
 
 export {
--- src/core/index.js
+++ src/core/index.js
@@ -2,6 +2,10 @@
 
 import {wrapHtml} from "./genHtml.js";
 
+// Constants
+const allowedProtos = ["http:", "https:", "ws:", "wss:"],
+failureCrits = ["client", "server", "loose", "asIs"];
+
 Array.prototype.random = function () {
 	return this[Math.floor(Math.random() * this.length)];
 };
@@ -25,18 +29,93 @@ let matchLang = eG("MATCH_LANG", "*").split(",");
 let maxTries = Math.max(parseInt(eG("HEALTH_MAX_TRIES", "3")), 1);
 let activeCheck = self.isPersPlat && Math.max(parseFloat(eG("HEALTH_ACTIVE", "5")), 15) * 1000;
 let failCrit = eG("HEALTH_CRITERIA", "asIs");
+let timeoutMs = Math.max(parseInt(eG("TIMEOUT_MS", "4000")), 4000);
+
+// Server console messages
+console.info(`Debug mode: ${debugHeaders ? "on" : "off"}`);
+console.info(`Backends: ${origin}`);
 
 let handleRequest = async function (request, clientInfo) {
 	// Generate a pre-determinted response if nothing is configured.
 	if (origin.length == 1 && origin[0] == "internal") {
-		return new Response(wrapHtml(`Hey, it works!`,`<span id="c">Cloud Hop</span> is now deployed to this platform. Please refer to the documentation for further configuration.`), {
-			status: 503,
-			headers: defaultHeaders
-		});
+		return wrapHtml(503, `Hey, it works!`,`<span id="c">Cloud Hop</span> is now deployed to this platform. Please refer to the documentation for further configuration.`);
 	};
 	let reqUrl = new URL(request.url);
-	let response, backTrace = [];
-	return response || new Response("Empty respose.", {status: 500});
+	// Give an error page when protocol mismatch
+	let detProtIdx = allowedProtos.indexOf(reqUrl.protocol);
+	if (detProtIdx == -1) {
+		return wrapHtml(400, `Unsupported`, `Protocol "${reqUrl.protocol}" is not supported by <span id="c">Cloud Hop</span>.`);
+	};
+	// Testing inbound TLS settings
+	switch (tlsIn) {
+		case "plain": {
+			if (detProtIdx % 2 == 1) {
+				return wrapHtml(400, `HTTPS only`, `Only HTTPS connections are allowed.`);
+			};
+			break;
+		};
+		case "tls": {
+			if (detProtIdx % 2 == 0) {
+				return wrapHtml(400, `HTTP only`, `Only HTTP connections are allowed.`);
+			};
+			break;
+		};
+	};
+	// Enforce outbound TLS
+	switch (tlsOut) {
+		case "tls":
+		case "plain": {
+			reqUrl.protocol = [(detProtIdx >> 1 << 1) + +(tlsOut == "tls")] || reqUrl.protocol;
+			break;
+		};
+	};
+	// Generate fake origin if it's set
+	// Match languages
+	// Passive health check
+	let response, backTrace = [], keepGoing = true;
+	while (maxTries >= 0 && keepGoing) {
+		// Give an error if tried too many times
+		if (maxTries <= 0) {
+			return wrapHtml(502, `Bad gateway`, `All origins are down${debugHeaders ? ": " + backTrace : ""}.`);
+		};
+		// Randomly choose an origin
+		let reqHost = origin.random();
+		let v6EndIdx = reqHost.lastIndexOf("]"),
+		portIdx = reqHost.lastIndexOf(":");
+		backTrace.push(reqHost);
+		reqUrl.hostname = reqHost;
+		reqUrl.port = "";
+		// Initiate a new request
+		let newReq = new Request(reqUrl.toString(), request);
+		// Send the request
+		response = await fetch(reqUrl, newReq);
+		// Test if the response matches criteria
+		switch(Math.floor(response.status / 100)) {
+			case 2: {
+				keepGoing = false;
+				break;
+			};
+			case 3: {
+				let redirLoc = response.headers.get("location");
+				response = wrapHtml(response.status, "Redirection", `Origin issued an redirect to: <a href="${redirLoc}">${redirLoc}</a>.`);
+				keepGoing = false;
+				break;
+			};
+			case 4: {
+				keepGoing = failureCrits.indexOf(failCrit) <= 0;
+				break;
+			};
+			case 5: {
+				keepGoing = failureCrits.indexOf(failCrit) <= 1;
+				break;
+			};
+			default: {
+				keepGoing = failureCrits.indexOf(failCrit) <= 2;
+			};
+		};
+		maxTries --;
+	};
+	return response || wrapHtml(500, "Empty response", `${keepGoing ? "Successful" : "Failed"} empty response from trace: ${backTrace}.<br/>Last requested URL: ${reqUrl.toString()}`);
 };
 
 export {

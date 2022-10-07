6f8de740945b8d7e14dbe3492f164a7048c0299a
PoneyClairDeLune <88174309+PoneyClairDeLune@users.noreply.github.com>
2022-10-07 14:56:56 +0000

    WebSocket client shim for CF Worker.

--- src/cloudflare/inject.js
+++ src/cloudflare/inject.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// Deno API shim
+
 let upgradeWebSocket = function (request) {
 	let [client, socket] = Object.values(new WebSocketPair());
 	socket.accept();
@@ -16,6 +18,49 @@ let Deno = {
 	upgradeWebSocket
 };
 
+// Other shims
+
+let WebSocket = class {
+	#target;
+	#url = "";
+	#getWsObj = async function (url) {
+		let reply = await fetch(url, {
+			headers: {
+				"Upgrade": "websocket"
+			}
+		});
+		let handle = reply.webSocket;
+		if (handle) {
+			this.#target = handle;
+			handle.accept();
+		} else {
+			throw Error("Bad WebSocket handshake");
+		};
+	};
+	addEventListener(...args) {
+		this.#target.addEventListener(...args);
+	};
+	close(...args) {
+		this.#target.close(...args);
+	};
+	send(...args) {
+		this.#target.send(...args);
+	};
+	get url() {
+		return this.#url;
+	};
+	constructor(url) {
+		let wsTarget = url,
+		protIdx = url.indexOf("ws");
+		if (protIdx == 0) {
+			wsTarget = protIdx.replace("ws", "http");
+		};
+		this.#url = url;
+		this.#getWsObj(wsTarget);
+	};
+};
+
 export {
-	Deno
+	Deno,
+	WebSocket
 };

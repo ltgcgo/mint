1b56003a6b3328fbaafa820ff16fe573f510eb5b
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-01-14 13:26:08 +0000

    Node.js support is almost ready. Almost.

--- sh/nodeTest.sh
+++ sh/nodeTest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LISTEN_PORT=8000
+export LISTEN_PORT=8005
 export BACKENDS=browserleaks.com
 export DEBUGGER=1
 export FOLLOW_REDIR=1
--- src/node/index.js
+++ src/node/index.js
@@ -6,12 +6,69 @@
 import {handleRequest} from "../core/index.js";
 //import {stripHeader} from "./strip.js";
 
+let headerObject = function (headers) {
+	let object = {};
+	headers?.forEach((v, k) => {
+		object[k] = v;
+	});
+	return object;
+};
+
 /* serve(async function (request, connInfo) {
 	let clientIp = connInfo.remoteAddr.hostname;
 	return await handleRequest(request, clientIp);
 }); */
-let server = http.createServer(function (request, response) {
-	response.writeHead(200, new Headers({"X-Platform": pV}));
-	response.write("Test body.");
-	response.end();
+let server = http.createServer(async function (requester, responder) {
+	// Request section
+	let clientIp = requester.headers["x-real-ip"] || requester.headers["x-forwarded-for"] || requester.socket.remoteAddress;
+	if (clientIp.indexOf("::ffff:") == 0) {
+		clientIp = clientIp.slice(clientIp.lastIndexOf("::ffff:") + 7);
+	};
+	let readStreamController;
+	let bodyStream = new ReadableStream({
+		type: "bytes",
+		start: (controller) => {
+			readStreamController = controller;
+		},
+		cancel: (reason) => {},
+		autoAllocateChunkSize: 65536
+	});
+	let reqOpt = {
+		"method": requester.method,
+		"headers": requester.headers
+	};
+	let bodyUsed = ["GET", "HEAD"].indexOf(reqOpt.method) == -1;
+	requester.on("data", (chunk) => {
+		readStreamController.enqueue(chunk);
+	}).on("end", () => {
+		readStreamController.close();
+	});
+	if (bodyUsed) {
+		reqOpt.body = bodyStream;
+		reqOpt.duplex = "half";
+	};
+	let request = new Request(`https://${requester.headers.host}${requester.url}`, reqOpt);
+	// Reply section
+	let response = await handleRequest(request, clientIp);
+	response?.headers?.forEach((v, k) => {
+		responder.setHeader(k, v);
+	});
+	responder.statusCode = response?.status || 200;
+	if (response?.statusText) {
+		responder.statusMessage = response.statusText;
+	};
+	responder.flushHeaders();
+	let repBodyStream = response.body.getReader(),
+	repBodyFlowing = true;
+	while (repBodyFlowing) {
+		await repBodyStream.read().then(({done, value}) => {
+			if (done) {
+				responder.end();
+				repBodyFlowing = false;
+			} else {
+				responder.write(value);
+			};
+		});
+	};
 });
+server.listen(parseInt(eG("LISTEN_PORT")) || 8006);

6dad82bfe9454a4ed4061fab7e5976aec35762b1
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-26 12:36:08 +0000

    Node.js now has WebSocket support.

--- src/openfaas/prefix.js
+++ src/openfaas/prefix.js
@@ -1,1 +1,1 @@
-"use strict";let eG=(a,b="")=>{return process.env[a]||b};let pV="DigitalOcean",pE=process.exit.bind();let self=globalThis;let pP=true;
+"use strict";let eG=(a,b="")=>{return process.env[a]||b};let pV="OpenFaaS",pE=process.exit.bind();let self=globalThis;let pP=true;
